14ba0efc97e6784383c7e63bc4de94aae2245058
Kawan Manzini <kawan.manzini@qodeless.io>
2024-04-02 22:05:58 -0300

    add assets and tailwind

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text, TextInput} from "react-native"
+
+export default function Home(){
+  return(
+      <View style={{backgroundColor: "#7A8D91", display: "flex", flex: 1, alignItems: "center"}}>
+        <Text style={{color: "#fff"}}>
+          Hello World!
+        </Text>
+      </View>
+  )
+}
