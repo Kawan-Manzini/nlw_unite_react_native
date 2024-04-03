c8dc5c94be587cea371ee72f7c3a02fbb4a43486
Kawan Manzini <kawan.manzini@qodeless.io>
2024-04-02 23:42:27 -0300

    Finished lesson 01

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,34 @@
-
-// Import your global CSS file
+// Importe suas folhas de estilo global CSS
 import "../styles/global.css"
 
-import { Slot } from 'expo-router'
-import { StatusBar } from "expo-status-bar"
+import React from 'react';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import {
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
-  useFonts
-} from '@expo-google-fonts/roboto'
-import { Loading } from '@/components/loading'
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { Loading } from '@/components/loading';
 
 export default function Layout() {
-  const [fontsLoaded] = useFonts([
+  const [fontsLoaded] = useFonts({
     Roboto_700Bold,
     Roboto_500Medium,
     Roboto_400Regular,
-  ])
+  });
 
+  if (!fontsLoaded) {
+    // Se as fontes não foram carregadas ainda, você pode renderizar um carregamento ou
+    // retornar null para evitar a renderização antes das fontes serem carregadas.
+    return <Loading />;
+  }
 
-
-  return (<>
-    <StatusBar style="light" />
-    {fontsLoaded ? <Slot /> : <Loading />  }
-    
-  </>)
+  // Se as fontes estiverem carregadas, renderize o Slot
+  return (
+    <>
+      <Slot />
+    </>
+  );
 }
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,23 +1,27 @@
 import { View, Text, Image, StatusBar } from "react-native"
 import { Input } from "@/components/input"
-import {MaterialCommunityIcons} from '@expo/vector-icons'
+import {FontAwesome6, MaterialIcons} from '@expo/vector-icons'
 import { colors } from "@/styles/colors"
 import { Button } from "@/components/button"
 import { Link } from "expo-router"
 
-export default function Home() {
+export default function Register() {
   return (
       <View className="bg-green-500 flex-1 items-center justify-center p-8 ">
         <StatusBar barStyle="light-content" />
         <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain"/>
         <View className="w-full mt-12 gap-3 ">
             <Input>
-            <MaterialCommunityIcons name="ticket-confirmation-outline" size={20} color={colors.green[200]}/>
-              <Input.Field placeholder="Codigo do ingresso"/>
+            <FontAwesome6 name="user-circle" size={20} color={colors.green[200]}/>
+              <Input.Field placeholder="Nome Completo"/>
+            </Input>
+            <Input>
+            <MaterialIcons name="alternate-email" size={20} color={colors.green[200]}/>
+              <Input.Field placeholder="E-mail" keyboardType="email-address"/>
             </Input>
             <Button title="Acessar credencial" />
-            <Link href="/register" className="text-gray-100 text-base font-bold mt-8 text-center" >
-              Ainda nao possui ingresso?
+            <Link href="/" className="text-gray-100 text-base font-bold mt-8 text-center" >
+              Ja possui ingresso?
             </Link>
         </View>
       </View>
