ef2160d00a2f23aec8cc264ebeb5be9aff169cc0
Kawan Manzini <kawan.manzini@qodeless.io>
2024-04-02 22:46:05 -0300

    setup imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,10 +4,26 @@ import "../styles/global.css"
 
 import { Slot } from 'expo-router'
 import { StatusBar } from "expo-status-bar"
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto'
+import { Loading } from '@/components/loading'
+
 export default function Layout() {
+  const [fontsLoaded] = useFonts([
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  ])
+
+
+
   return (<>
     <StatusBar style="light" />
-
-    <Slot />
+    {fontsLoaded ? <Slot /> : <Loading />  }
+    
   </>)
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar"
 import { View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function Home() {
   return (
-      <View className="bg-green-500 flex-1 items-center ">
-        <Text className="text-white">
+      <View className="bg-green-500 flex-1 items-center justify-center  ">
+        <Text className="text-orange-500 items-center text-2xl">
           Hello World!
         </Text>
       </View>
